5f0caa36a9f079b27c4ec5d892640f27b24a7f75
sms890522-coder <sms890522@nate.com>
2026-04-23 10:59:24 +0000

    커밋

---
+++ data.js
@@ -0,0 +1,17 @@
+const churchData = {
+  name: "우리교회",
+  slogan: "사랑과 은혜가 흐르는 공동체",
+
+  services: [
+    { title: "주일예배", time: "오전 11:00" },
+    { title: "수요예배", time: "오후 7:30" },
+    { title: "새벽기도", time: "오전 5:30" }
+  ],
+
+  announcements: [
+    { title: "부활절 예배 안내", date: "2026-04-20" },
+    { title: "새가족 환영회", date: "2026-04-15" }
+  ],
+
+  youtube: "https://youtube.com/"
+};
